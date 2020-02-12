2ae7e49741716150ad5ffa95cdde992a5da6cf09
David Fernando <30445601+david-fernando@users.noreply.github.com>
2020-02-12 00:50:03 -0300

    Delete fileTypeOptions.js

--- src/utils/fileTypeOptions.js
+++
@@ -1,9 +0,0 @@
-const fileTypeOptions = [
-    {key: 0, value:'default', text: 'Tipo de arquivo'},
-    {key: 'mp3', value: 'mp3', text: 'MP3'},
-    {key: 'mp4', value: 'mp4', text: 'MP4'},
-    {key: 'jpg', value: 'jpg', text: 'JPG'},
-    {key: 'png', value: 'png', text: 'PNG'},
-  ]
-
-export default fileTypeOptions;
